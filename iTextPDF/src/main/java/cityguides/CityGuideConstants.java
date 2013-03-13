bb5f7456217ff96d50496e47506664dad572c015
dshevani <aum2deepak@gmail.com>
2013-03-13 10:42:48 +0530

    Remove

--- iTextPDF/src/main/java/cityguides/CityGuideConstants.java
+++
@@ -1,8 +0,0 @@
-package cityguides;
-
-public class CityGuideConstants {
-
-	public static final String SLOGAN = ".. serving your travel needs";
-	public static final String API_URL = "http://internalapi.travel.yahoo.com/v1/entity?id=%s&lang=en-US&view=full&format=json&product=travelguide";
-	
-}
